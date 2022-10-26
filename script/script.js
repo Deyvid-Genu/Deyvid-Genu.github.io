6050c06a461b5e8767bec72409b007f0323696f2
Deyvid Genu <deyvid.genu@gmail.com>
2022-10-26 12:51:50 -0300

    Active nav link fixed

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const local = location.href;
 const url = document.querySelectorAll(".header-menu a");
 
 url.forEach((element) => {
-  if (local.includes(element.href)) {
+  if (local.includes(element.href) || local === url) {
     element.classList.add("ativo");
   }
   console.log(element.href);
